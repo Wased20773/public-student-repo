70a8c676c3e3753573fea93fba1368c65592208f
Bryan Vazquez <gamer.brybry@gmail.com>
2025-10-27 21:04:08 -0700

    Getting started with 02-search

--- hw2/02-search.js
+++ hw2/02-search.js
@@ -1,3 +1,9 @@
 // Add your code here
+const userInput = document.querySelector("input[type='button']");
+const userValue = document.querySelector("input[type='text']");
 
-userInput.addEventListener('click', handleClick);
+const handleClick = function SearchForDataInSearchBar(event) {
+  console.log(userValue.value);
+};
+
+userInput.addEventListener("click", handleClick);
