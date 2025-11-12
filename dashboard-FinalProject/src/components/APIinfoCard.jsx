1a7ed6f67d4b4360448aaa1c77f319cdf82ff6da
Bryan Vazquez <gamer.brybry@gmail.com>
2025-11-11 23:10:10 -0800

    +Added official API doc link to API info cards, slight redesign to linear gradient color (light to dark)

--- dashboard-FinalProject/src/components/APIinfoCard.jsx
+++ dashboard-FinalProject/src/components/APIinfoCard.jsx
@@ -5,16 +5,24 @@ import "../styles/home.css";
 const APIinfoCard = ({ api, theme, width_img }) => {
   return (
     <div className={`cards-api ${theme}`}>
-      <div className="cards-api-heading">
-        <img
-          src={api.img}
-          width={width_img}
-          alt={api.alt}
-          style={{ borderRadius: "50%" }}
-        />
-        <h3 className="cards-api-h3">{api.name}</h3>
+      <div>
+        <div className="cards-api-heading">
+          <img
+            src={api.img}
+            width={width_img}
+            alt={api.alt}
+            style={{ borderRadius: "50%" }}
+          />
+          <h3 className="cards-api-h3">{api.name}</h3>
+        </div>
+        <div>{api.info}</div>
       </div>
-      <p>{api.info}</p>
+      <a
+        href={api.documentation[0].url}
+        className="cards-api-documentation-link"
+      >
+        Official Documentation
+      </a>
     </div>
   );
 };
